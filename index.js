098d80570b52207ba0a1fb73b1de4436ad7a27a0
梁柱 <liangzhu@77ircloud.com>
2019-04-25 11:52:09 +0800

    update prettier

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // 基于 React 事件的, 内聚的事件管理器
-import React from "react";
+import React from 'react';
 
 // eslint-disable-next-line
 class Event extends React.PureComponent {
   static defaultProps = {
     // 用于配置其他触发onChange的键
-    keys: ["value", "onChange", "onClick", "onTouchEnd"]
+    keys: ['value', 'onChange', 'onClick', 'onTouchEnd']
   };
 
   constructor(props) {
     super(props);
 
     this.state = {
-      value: this.props.defaultValue || "",
+      value: this.props.defaultValue || '',
       // 用于update时, 更新其他props
       otherProps: {}
     };
@@ -34,7 +34,7 @@ class Event extends React.PureComponent {
   componentDidMount() {
     const { onDidMount, name } = this.props;
 
-    if (typeof onDidMount === "function") {
+    if (typeof onDidMount === 'function') {
       // 用于在渲染后,就获得refs对象结构
       onDidMount({
         ref: this.ref,
@@ -55,7 +55,7 @@ class Event extends React.PureComponent {
     let value = e;
 
     // 如果是DOM对象, 根据类型取值
-    if (typeof e === "object") {
+    if (typeof e === 'object') {
       if (e.target) {
         if (e.target.files) {
           value = e.target.file[0];
@@ -70,7 +70,7 @@ class Event extends React.PureComponent {
       this.setState({ value });
     }
 
-    if (typeof onEvent === "function") {
+    if (typeof onEvent === 'function') {
       // 统一回调到Form的onChange中
       onEvent({
         ref: this.ref,
@@ -82,7 +82,7 @@ class Event extends React.PureComponent {
       });
 
       // 如果子注册了相应的事件函数, 也同事响应它, 并且传递其他状态数据
-      if (children.props && typeof children.props[eventName] === "function") {
+      if (children.props && typeof children.props[eventName] === 'function') {
         children.props[eventName](e, {
           ref: this.ref,
           value,
@@ -114,7 +114,7 @@ class Event extends React.PureComponent {
     const { children, keys } = this.props;
 
     // 根据 children 类型执行不同的渲染方式, 只支持 fn children 和单 children
-    if (typeof children === "function") {
+    if (typeof children === 'function') {
       return children({
         ...otherProps,
         ...this.onEvents,
@@ -146,7 +146,7 @@ export default class extends React.PureComponent {
   componentDidMount() {
     const { onDidMount } = this.props;
 
-    if (typeof onDidMount === "function") {
+    if (typeof onDidMount === 'function') {
       onDidMount(this.formDatas);
     }
   }
@@ -159,7 +159,7 @@ export default class extends React.PureComponent {
 
     const { onUnMount } = this.props;
 
-    if (typeof onUnMount === "function") {
+    if (typeof onUnMount === 'function') {
       onUnMount(this.formDatas);
     }
   }
@@ -167,7 +167,7 @@ export default class extends React.PureComponent {
   handleOnTrigger = value => {
     const { onTrigger } = this.props;
 
-    if (typeof onTrigger === "function") {
+    if (typeof onTrigger === 'function') {
       onTrigger(value, this.formDatas);
     }
   };
@@ -207,7 +207,7 @@ export default class extends React.PureComponent {
       }
     }
 
-    if (!isFromDidMount && typeof onEvent === "function") {
+    if (!isFromDidMount && typeof onEvent === 'function') {
       onEvent(this.formDatas);
     }
   };
